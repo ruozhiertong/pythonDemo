1d5e98649bca31a4ebe895223e59207c7857abe0
rzet <rzet@test.com>
2020-11-25 16:53:45 +0800

    update

--- cvDemo/badapple3.py
+++ cvDemo/badapple3.py
@@ -1,14 +1,14 @@
 #! encoding: UTF-8
 import time  # 引入time模块
-import showPicVideo
+import showVideoOnPi
 
 # 直接使用opencv 处理视频，在此基础上 直接显示图片，从而播放。
-
 filepath = "resource/badapple.mp4"
 
 def main():
     start = time.time()
-    showPicVideo.process(filepath)
+    disp = showVideoOnPi.initSSD1306()
+    showVideoOnPi.process(filepath,disp.width, disp.height)
     end = time.time()
     print(end - start)
 if __name__ == "__main__":
--- cvDemo/showVideoOnPi.py
+++ cvDemo/showVideoOnPi.py
@@ -7,26 +7,25 @@ import cv2
 import time  # 引入time模块
 import Adafruit_SSD1306
 
-#Raspberry Pi pin configuration:
-RST = None     # on the PiOLED this pin isnt used
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
+def initSSD1306():
+    #Raspberry Pi pin configuration:
+    RST = None     # on the PiOLED this pin isnt used
+    disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
+    # Initialize library.
+    disp.begin()
+    # Clear display.
+    disp.clear()
+    disp.display()
+    # Create blank image for drawing.
+    # Make sure to create image with mode '1' for 1-bit color.
+    w = disp.width #128
+    h = disp.height #64
+    # image = Image.new('1', (width, height)) #创建一个图片
+    # Get drawing object to draw on image.
+    # draw = ImageDraw.Draw(image)
+    return disp
 
-# Initialize library.
-disp.begin()
-
-# Clear display.
-disp.clear()
-disp.display()
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-w = disp.width #128
-h = disp.height #64
-# image = Image.new('1', (width, height)) #创建一个图片
-# Get drawing object to draw on image.
-# draw = ImageDraw.Draw(image)
-
-
-def process(filepath,width=w,height=h,x=0,y=0):
+def process(filepath,width,height,x=0,y=0):
     cap = cv2.VideoCapture(filepath)
     #处理每一帧
     while(True):
@@ -65,7 +64,8 @@ def main():
     if(sys.argv.size() == 1)
          sys.exit()
     start = time.time()
-    process(sys.argv[1],w, h)
+    disp = initSSD1306()
+    process(sys.argv[1],disp.width, disp.height)
     end = time.time()
     print(end - start)
 if __name__ == "__main__":
